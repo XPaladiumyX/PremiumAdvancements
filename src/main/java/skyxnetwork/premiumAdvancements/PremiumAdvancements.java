4c266cb63ab7506cfcc740aae82062a324c8b901
XPaladiumyX <allegra.yann@gmail.com>
2024-12-18 20:12:31 +0100

    first commit

---
+++ src/main/java/skyxnetwork/premiumAdvancements/PremiumAdvancements.java
@@ -0,0 +1,17 @@
+package skyxnetwork.premiumAdvancements;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class PremiumAdvancements extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
